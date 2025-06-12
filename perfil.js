61335c1e0ed2a1fd17b737e8d2d8f55bb486c616
ssanbel <alessandraguerrero9@gmail.com>
2025-06-11 21:24:43 -0500

    Add files via upload

---
+++ perfil.js
@@ -0,0 +1,26 @@
+// Mostrar nombre de usuario en el menú si hay sesión iniciada
+window.addEventListener("DOMContentLoaded", () => {
+  const usuario = JSON.parse(localStorage.getItem("usuarioDatos"));
+  const userNameDisplay = document.getElementById("userNameDisplay");
+
+  if (usuario && userNameDisplay) {
+    userNameDisplay.textContent = usuario.nombres;
+  }
+
+  // Mostrar datos en el perfil
+  if (usuario) {
+    document.getElementById("nombreUsuario").textContent = usuario.nombres;
+    document.getElementById("apellidoUsuario").textContent = usuario.apellidos;
+    document.getElementById("dniUsuario").textContent = usuario.dni;
+  }
+
+  // Cerrar sesión
+  const btnCerrarSesion = document.getElementById("cerrarSesionBtn");
+  if (btnCerrarSesion) {
+    btnCerrarSesion.addEventListener("click", () => {
+      localStorage.removeItem("usuarioLogueado");
+      localStorage.removeItem("usuarioDatos");
+      window.location.href = "index.html";
+    });
+  }
+});
